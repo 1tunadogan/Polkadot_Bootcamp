1b4d1f427f1b5ebf73b267353174b1d9bb19fbac
1tunadogan <1tunadogan@gmail.com>
2023-06-21 02:11:15 +0300

    Remove task_1/target from repository

--- task_1/target/debug/deps/task_1-8a6d4a1cec6b56c0.d
+++
@@ -1,5 +0,0 @@
-/home/tuna/Polkadot_Bootcamp/task_1/target/debug/deps/task_1-8a6d4a1cec6b56c0.rmeta: src/main.rs
-
-/home/tuna/Polkadot_Bootcamp/task_1/target/debug/deps/task_1-8a6d4a1cec6b56c0.d: src/main.rs
-
-src/main.rs:
--- task_1/target/debug/task_1.d
+++
@@ -1,1 +0,0 @@
-/home/tuna/Polkadot_Bootcamp/task_1/target/debug/task_1: /home/tuna/Polkadot_Bootcamp/task_1/src/main.rs
